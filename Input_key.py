5ad925f7d0df54da41d3deb31bdc2df218e3807e
yurifillippo <60762134+yurifillippo@users.noreply.github.com>
2024-09-06 17:58:34 -0300

    Update Input_key.py

--- Input_key.py
+++ Input_key.py
@@ -1,20 +1,19 @@
 # Databricks notebook source
 json_data = {
   "type": "service_account",
-  "project_id": "datamaster-434121",
-  "private_key_id": "5fffab8eca162d358ad98f52d768028009cd5ccd",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCV4sqJaOvXA8My\n7JveMSDPiLexWE9mDp6BO+i4wjHwPJGfJYyxCNnWylf5httIRpUv+p0wq5dkvRru\n7xsb43SpZPHtCWkBpDJY5e9hkM9JQShUaYjK0o9BH5eZy7rEu8FExSlcBgDhwGFm\nh9sr+9QaIgxif4bxRmG87vytb+a/wQHq6TbehTM4RTHsG4qFap63FkwNsAw+YNXe\nJquMjlLBBwQ0y0H1lzCPmSJwy2by5kQzGBjCu70u/xLuNmDWt/I3OS+UCU2ITEor\nBBBnPoBOhZ5x021HcqE4Tlt/xuA5w6DZREPXz6tuhQNtT+3XYdmgxQPgWZfR9swm\nincFtzvdAgMBAAECggEAC943jc5QQEf0vJNGGz+WGsC3VNRKgyj8rXx2Jqz/wcc1\n+51ASONLxmlismgWdGBcXz5vlGM921Tq7UjjA2ANBH2w90UjuqTK9MWOIRJTi3gc\njqSFl5O5Ep3DBgM+8GVyffTDm90A1F9EhxcSSGcKCiCwjNsOrwuU4R4mRUBjS8MI\nSx7hs4kmos29XKwF4JAUNaRaayqaUeVm68WXI1uVjNNC4YszhhY9JcmSrUf5M+im\nMiZH76W66Uvuv5y964s4JKloOq5e9t3Kn06JPcN3v65gdJireRRZN6nIb1cM+jwe\nkDkj4MQcXLb65TINH7KFtMWKsUOpXcjcXAKjY7DCQQKBgQDNOATUk2nwOOTdnyCL\ne63P+3vR/P2ePjCjoVjcDHs05ShdumhlRtmztFBptLgB1m/qCiVr5byz8V8UgNS+\nnT5stzvMQD6hPhtk2GwyLxa+mHMU72/jyXBJyb25sEq7Br/BcJ5vms78SA4EAUs+\n5g/KCslVEWKRrTLjCc+uAE4aHQKBgQC6+ZocJXK+sHmEkJi/ZQFatCBc6JnJ5TeL\nI46ucASqq92tmpvWiezMcPxbNPuhNEweQVgZYvN9RSGZ4ojpzfjEHxXf7/+5JED6\nlIfuYr5rFn/aDwfDLLFiWJ/MC6vu3q0Ol0tPj/5F26v87y80bMGYPEWJJwf2nmic\n/F4Ru9X8wQKBgBQYBRkPahMUbwxoNVaTAJzZwfD4tZiV/Es5VCsPqcyvF3m989Te\n0BgxcqI3CMFdTs36ullLQGPaIXYveyVC3kbk1h0UuU0ueJ4yNr3fMHnvEW2eWDVU\nlwMSltoksdyIN6RoM4s3/EZlg8HOoqCBBFDDJ52Fu0IKVG44mrYt33fdAoGBAKzK\nQI5kWS86fYJx8odl+6NsNUBHQOBg8TynhpNUOCvVmo52BDB21Bx3Ce/r/eMTJokL\nUDRdyrFo8s14mZigXZY5OAj0jXn2tAoeu6QlIt5qM4s8Oqs3IVLGnh5+ZszggkOq\ns9F6O70pbj7Yd+JV428hvA0sweI6sGjrnK98zBvBAoGBAIiX616/eWRbzcCjKZeA\n5194LG0GvxSg51sXxU3Kl7GLCzepPW3RBXCS0+z/bp9abzXq95Kigkll94Euvxej\n0q4Ua21g5mcLm0kL72Sn18/B9VCwLlOMwZ+riq9hNYRyobLpb/+e8kTE16Ir8H0j\nAtD/baDIo1+XtD3UdaVBtjHm\n-----END PRIVATE KEY-----\n",
-  "client_email": "datamaster-434121@appspot.gserviceaccount.com",
-  "client_id": "114142912056624525312",
+  "project_id": "datamaster01",
+  "private_key_id": "11d1b079162364785491ad2d770c8b6ff390a2cb",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDLZBGu1fibmnOB\noN7WuKWuhcN/1ie9Qn1zfJSdApKehF92iU60lo669WqeOxvGUw/0M7qxhcjGdkuB\nlmB/FB5P0vH5mzDXUiSt/KVNMtujdL7axbR/hvQZ/HwkHwdLZH6JxC2DNTkI/BEB\nEPvNcTZ+12V+enVR+lv7wlaea8OQFew8VJuSibBg54A7pEKoz1Wt3UwH3EnadyMl\nUQWveJGrKGEvJFOpAKDrvhrFSdKbcFT7K66GXNOmsQRfG5yfkx1XnTEcdA7iTtNJ\naz+mfjPMNGQD5GUa/jw8u7TK8XvBNtxTfZH90+hojGZRuqY1wUoAWK2CKmNtev+7\n2/i3tWM9AgMBAAECggEAM3e3Ic7zgiyrXfofaaaABpHCzu6aT8IrjLXRxYmJIKPO\npGhKilgu9hB0UmYSuTT7rIgKjjGUhOQaZ0huUrn8kaaHpOooidQ3g++SxN5BxjMc\nuK+e5UZ5Lro9j8ZqSiG8A5CpE7K8JBHlG6f4kBw99gr7m+3RElWQQT0EPkJ20lbG\nJ1LgB6UzS1IGQOvpDZR2JmW80X749eQ+iYh6JJysNag3MDaLbMopVK+oFPjsintZ\nfSfJLOZMJceOv0dXFF0ij16cVdToVybH3K2Ahbbn8L4CFpIfSaG/BWcdXCjt5JLv\nEi6mh5Jr/GilGMrf19uNsaMIkytZR01Ldaqubr2t4QKBgQDyUdvp2ieG+vYvVrch\njhCyBVMGXRG8DwGL9qMbIdLYnQz9BOXCYHiubEHPjj8NLVkn/6GKnedxa4vn+phj\n4VGMP9fz2JyvTWPQxfX7mfhSBlSXGxiHvHGcJK226MLpySiOfF81MAH7xwdXOEwc\nFCgxM63hjr5jTveBDGXJuazs+wKBgQDW35aW4iqrDqzA20Kt3OnN2UJ5LOTR4zaK\nfzYx+heR/tlfGF973h4kp1u93jxEssEKRfxliKLvsMaxV7LqRKymwgxv86zrj0A8\nIZG2WnShEXu6UJasKU7eeIf1rrZBvMRGmUi1n19Esvv1g6LEYKZ4y+o82cxhJLXW\nUcJwTEOLJwKBgBSTiOYBpUub8d2xMlnCE37aXuNycbgTWiFFbzI1nA7SvzCZAt2P\njY/aF3iFbqsyx5hS56e6otWJuVaYe+o3TtJm9XJ6WUu1eZ1Xwfx8ZY6phPNT1LBp\n+we0QZ//gdWSRERdIUSF8BUuIFxyplvYBlWvOrulgFs0cGXf5KDNiHaTAoGAOTZr\neF1cExPjN66qAHUz49WXd4BWpPQkz5ezrHb007DR3Bo3QloQGJ5fNRz5WyllcQDV\nhYtuJvggz5OUVgBXIEfG3AA76LTJ27jsfWt0ZnarRR60H78X+vCI8wfEC5jhghLK\nY2G+EiK+J88XBcbeTIevJOixHKSTri8y4IfN7D0CgYEAxf8NTOjUrBIKkUoMhyQC\nxYn1JdPkrBmvGIYch/0BRy63Yh8pRdLxN5LqPVoK3clu1WHb7dnDG5vThFoH2G0S\nruRrosjSvPrdO8XId6I0k6uUyCnXY000E9FTzdx8Xr/8BwRlMLEFnZF/BEoIDdyQ\nX+k8xekoCljXaM+pgO02DkY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "data-master-account@datamaster01.iam.gserviceaccount.com",
+  "client_id": "115579238823557687236",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/datamaster-434121%40appspot.gserviceaccount.com",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/data-master-account%40datamaster01.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
 }
 
-
-# COMMAND ----------
+#####################################################
 
 import json
 
@@ -34,7 +33,9 @@ print(f"Arquivo JSON salvo em {file_path}")
 # Listar arquivos na pasta onde o arquivo foi salvo
 dbutils.fs.ls("/FileStore/keyfiles/")
 
-# COMMAND ----------
+
+#####################################################
+
 
 file_contents = dbutils.fs.head("dbfs:/FileStore/keyfiles/your_key.json")
 print(file_contents)
